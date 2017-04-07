4925a6f45f2cbf2dc1eedc970e20dee9f22d5b28
chenw <chenw@shuwang.info>
2017-04-07 17:19:36 +0800

    adjust some config

--- demo/src/main/java/com/shuwang/receipt/demo/Application.java
+++ demo/src/main/java/com/shuwang/receipt/demo/Application.java
@@ -8,7 +8,7 @@ public class Application {
     	System.out.println( "欢迎使用福建数网聚合收款SDK" );
     	String gatewayurl = "http://api.test.shuwang.info/receipt/rest";
     	String appid = "12345";
-    	String appsecret = "";
+    	String appsecret = "12345";
     	String method = "receipt.scan.jsapi";
     	String body = "{}";
         ReceiptCall receiptCall = new ReceiptCall();
